5b42051cadfccd056c02d59e6c0b0719e3b02f1d
youssefimajat <95992103+youssefimajat@users.noreply.github.com>
2022-07-05 23:58:36 -0500

    Delete src directory

--- src/main/java/cs425/miu/MyStudentMgmtApp/repository/ClassroomRepository.java
+++
@@ -1,9 +0,0 @@
-package cs425.miu.MyStudentMgmtApp.repository;
-
-import cs425.miu.MyStudentMgmtApp.model.Classroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClassroomRepository extends JpaRepository<Classroom,Long> {
-}
--- src/main/java/cs425/miu/MyStudentMgmtApp/service/CourseService.java
+++
@@ -1,8 +0,0 @@
-package cs425.miu.MyStudentMgmtApp.service;
-
-import cs425.miu.MyStudentMgmtApp.model.Course;
-
-public interface CourseService {
-    public abstract Course addNewCourse(Course course);
-    public Course updateCourse(Course updatedCourse);
-}
